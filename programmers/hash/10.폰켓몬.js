b83ff2a9764007a253cb6ebd463adba12d024fe7
gyduddl <gyduddl@naver.com>
2024-03-16 00:06:59 +0900

    20240316

--- programmers/hash/10.폰켓몬.js
+++ programmers/hash/10.폰켓몬.js
@@ -1,4 +1,14 @@
 function solution(nums) {
     var answer = 0;
+    let set = new Set(nums);
+    console.log(set.size);
+    if (set.size <= nums.length / 2) {
+        answer = set.size;
+    } else {
+        answer = parseInt(nums.length / 2);
+    }
     return answer;
 }
+
+// 배열에서 총 몇 종류가 있는지 파악
+// 그것이 num/2 작으면 그것 출력 크면 num/2출력
