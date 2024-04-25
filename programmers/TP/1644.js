b97335a72870ab696a172266d60c53d01ed03604
gyduddl <gyduddl4889@naver.com>
2024-04-25 13:09:07 +0900

    1644-2

--- programmers/TP/1644.js
+++ programmers/TP/1644.js
@@ -1,3 +1,6 @@
+//**소수의 연속합 */
+
+//**메모리 27044 시간 4148 */
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = Number(fs.readFileSync(filePath).toString().trim().split('\r\n'));
@@ -9,26 +12,34 @@ let count = 0;
 
 function isPrime(num) {
     if (num <= 1) return false;
-
-    for (let i = 3; i < Math.sqrt(num); i++) {
+    if (num % 2 === 0) {
+        return num === 2 ? true : false;
+    }
+    for (let i = 3; i <= parseInt(Math.sqrt(num)); i++) {
         if (num % i === 0) {
             return false;
         }
     }
-    if (num === 2) return true;
+
     return true;
 }
 
-let arr = Array.from({ length: input }, (v, i) => {
-    if (isPrime(i)) return i + 1;
-    else return null;
-});
-console.log(arr);
+let arr = [];
+for (let i = 1; i <= input; i++) {
+    if (isPrime(i)) arr.push(i);
+}
+while (right <= arr.length) {
+    if (sum === input) {
+        count++;
+        sum -= arr[left];
+        left++;
+    } else if (sum < input) {
+        sum += arr[right];
+        right++;
+    } else if (sum > input) {
+        sum -= arr[left];
+        left++;
+    }
+}
 
-// if(sum===input){
-//     count++;
-//     sum -=arr[left];
-//     left++
-// }else if(){
-
-// }
+console.log(count);
